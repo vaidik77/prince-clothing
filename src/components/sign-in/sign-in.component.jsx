07a4deb1a485a7f5ee6c86e3202ceb7bf5f46e4c
vaidik77 <50943011+vaidik77@users.noreply.github.com>
2020-04-28 00:55:31 +0530

    finishing sign in hadleSubmit methods

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import './sign-in.style.scss'
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component'
-import { signInWithGoogle } from '../../firebase/firebase.utiils'
+import {auth, signInWithGoogle } from '../../firebase/firebase.utiils'
 
 class SignIn extends React.Component{
     constructor(props){
@@ -14,10 +14,18 @@ class SignIn extends React.Component{
         }
     }
     
-    handleSubmit = event=>{
+    handleSubmit =async event=>{
         event.preventDefault();
-        this.setState({email: '',password:''})
-    }
+
+        const {email , password}=this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '',password:'' });
+        } catch(error){
+            console.log(error);
+        }
+
+    };
     handleChange=event=>{
         const {value,name}=event.target;
         this.setState({[name]:value})
